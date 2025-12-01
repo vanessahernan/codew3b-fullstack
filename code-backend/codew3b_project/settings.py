95dbe75360f2efcaa4ad74cb4123c623d5a07fc4
vanessahernan <hernamdezvanessa16@gmail.com>
2025-12-01 01:50:00 -0400

    Configure CORS for Netlify frontend

--- code-backend/codew3b_project/settings.py
+++ code-backend/codew3b_project/settings.py
@@ -38,6 +38,18 @@ INSTALLED_APPS = [
     # --------------------------------------------------
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    # Reemplaza 'tusitio.netlify.app' con el nombre que te dio Netlify
+    "https://tusitio.netlify.app", 
+]
+
+# 2. Lista de HOSTS permitidos (Tu dominio de Render y Netlify)
+ALLOWED_HOSTS = [
+    # Permite todos los subdominios de Render
+    '.onrender.com', 
+    'tusitio.netlify.app', 
+]
+
 MIDDLEWARE = [
     # --- DEBE SER EL PRIMERO para manejar las peticiones de Vue ---
     'corsheaders.middleware.CorsMiddleware', 
